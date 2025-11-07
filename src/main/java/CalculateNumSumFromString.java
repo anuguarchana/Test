81fe9437e1ffc7d7e233034b15fbaee7aae78749
anuguarchana <33023322+anuguarchana@users.noreply.github.com>
2025-11-07 21:59:23 +0000

    Update CalculateNumSumFromString.java

--- src/main/java/CalculateNumSumFromString.java
+++ src/main/java/CalculateNumSumFromString.java
@@ -4,30 +4,31 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class CalculateNumSumFromString {
-
-    public static void main(String args[]) {
-        CalculateNumSumFromString calculateNumSumFromString = new CalculateNumSumFromString();
-        Integer maxNum1 = calculateNumSumFromString.calculateSum(List.of("dh7js4jf", "or2rjvn2w", "h1n36mfl", "a7e6fw"));
-        Integer maxNum2 = calculateNumSumFromString.calculateSum(List.of("dh7js5jf", "or2rjvn4w", "h1n37mfl", "a7e8fw"));
-        System.out.println(maxNum1);
-        System.out.println(maxNum2);
-    }
-
-    private Integer calculateSum(List<String> listOfInputs) {
-        Set<Integer> setOfNumbers = listOfInputs.stream()
-                .map(this::calculateStringNumbers)
-                .collect(Collectors.toSet());
-        return Collections.max(setOfNumbers);
+    
+    public int calculateSum(List<String> listOfInputs) {
+        if (listOfInputs.size() < 11) {
+            Set<Integer> setOfNumbers = listOfInputs.stream()
+                    .map(this::calculateStringNumbers)
+                    .collect(Collectors.toSet());
+            return Collections.max(setOfNumbers);
+        } else {
+            throw new IllegalArgumentException("Input array size must not exceed 10 elements");
+        }
     }
 
     private int calculateStringNumbers(String inputValue) {
-        int stringSum = 0;
-        for (char numAsChar : inputValue.toCharArray()) {
-            if (Character.isDigit(numAsChar)) {
-                int digit = Character.getNumericValue(numAsChar);
-                stringSum = stringSum + digit;
+        if(inputValue.length() <13) {
+            int stringSum = 0;
+            for (char numAsChar : inputValue.toCharArray()) {
+                if (Character.isDigit(numAsChar)) {
+                    int digit = Character.getNumericValue(numAsChar);
+                    stringSum = stringSum + digit;
+                }
             }
+            return stringSum;
         }
-        return stringSum;
+        else {
+            throw new IllegalArgumentException("Input String size must not exceed 12 char");
+        }
     }
 }
